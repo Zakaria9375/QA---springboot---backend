22a99466ae44065f2c98f46d59cbb144cb558cd6
Zakaria Ali <alizakaria9375@gmail.com>
2024-07-01 12:31:04 +0300

    refactor deps, users

--- src/main/java/com/zaportfolio/qa/dao/UserRepository.java
+++ src/main/java/com/zaportfolio/qa/dao/UserRepository.java
@@ -1,11 +1,17 @@
 package com.zaportfolio.qa.dao;
 
 import com.zaportfolio.qa.entity.User;
+import com.zaportfolio.qa.projections.UserProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.Optional;
+
 @CrossOrigin("http://localhost:5173")
+@RepositoryRestResource(excerptProjection = UserProjection.class)
+
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(@Param("email") String email);
+    Optional<UserProjection> findByEmail(@Param("email") String email);
 }
